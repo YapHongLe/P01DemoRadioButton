bcbdd515c7ee2d0d8656da3dff1074eb44a3be99
Yap Hong Le <18045404@myrp.edu.sg>
2020-04-24 11:48:29 +0800

    Hi

--- app/src/main/java/com/example/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/example/demoradiobutton/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.demoradiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+                int selectButtonId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selectButtonId);
+                Toast.makeText(MainActivity.this, rb.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
